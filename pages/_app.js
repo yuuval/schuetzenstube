4da5a8a2b6b0a21d93509d98dde4143558e2ff26
rawphl <ivancammarota@gmail.com>
2022-04-19 07:12:58 +0200

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import Header from "@components/Header"
+import useSession from "@lib/session"
+import Link from "next/link"
+import "./_app.css"
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }) {
+    const session = useSession()
+    const newPageProps = {
+        ...pageProps,
+        session
+    }
+    return (
+        <>
+            <Header>
+                <Link href="/" passHref>
+                    app
+                </Link>
+            </Header>
+
+            <main className="page">
+                <Component {...newPageProps} />
+            </main>
+        </>
+    )
 }
-
-export default MyApp
