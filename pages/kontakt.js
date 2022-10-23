d716efa640d1fcf6d71f8f3b78d17b3b29d02e4f
valmir__ <bhaliv@bbcag.ch>
2022-10-23 20:28:35 +0200

    kontakt finished

--- pages/kontakt.js
+++ pages/kontakt.js
@@ -1,8 +1,9 @@
 import {useRouter} from "next/router";
-import {useState} from "react";
+import {useRef, useState} from "react";
 import styles from "./KontaktForm.module.css"
 import Form from 'react-bootstrap/Form';
 import {isAfter, isBefore, addDays, format} from 'date-fns'
+import emailjs from "@emailjs/browser";
 
 const defaultModel = {
     title: "", message: "", reservationTime: "", email: ""
@@ -14,8 +15,8 @@ function validateModel(formular) {
     }
     let isValid = true;
 
-    if (formular.title.trim().length < 3) {
-        errors.title = "Titel darf nicht < 3 Zeichen sein"
+    if (formular.title.trim().length < 5) {
+        errors.title = "Titel darf nicht < 5 Zeichen sein"
         isValid = false;
     }
     if (formular.message.trim().length === null || formular.message.trim().length < 5) {
@@ -24,8 +25,13 @@ function validateModel(formular) {
     }
     //const inThreeDays = addDays(new Date(),3)
     //console.log(format(inThreeDays, "yyyy-MM-dd").charAt(10))
-    if (isBefore(formular.reservationTime, new Date()) === false) {
-        errors.reservationTime = "Reservationszeit darf kleiner "
+    let date = new Date();
+    /* Date format you have */
+    let dateMDY = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+    console.log(dateMDY)
+    /* Date converted to MM-DD-YYYY format */
+    if (isBefore(new Date(dateMDY), new Date(formular.reservationTime)) === false) {
+        errors.reservationTime = "Reservationszeit muss mindestens einen Tag später gewählt werden"
         isValid = false
     }
     if (formular.email.trim().length === 0) {
@@ -40,6 +46,7 @@ export default function KontaktForm() {
     const [formular, setFormular] = useState(defaultModel)
     const [isLoading, setIsLoading] = useState(false)
     const [errors, setErrors] = useState(defaultModel)
+    const form = useRef();
 
     const handleChange = (e) => {
         const field = e.target.name
@@ -55,7 +62,6 @@ export default function KontaktForm() {
         setErrors(defaultModel)
 
         const result = validateModel(formular)
-        const dateNow = format(new Date(), "yyyy-MM-dd")
         if (!result.isValid) {
             setErrors(result.errors)
             setIsLoading(false)
@@ -63,7 +69,14 @@ export default function KontaktForm() {
         }
         if(result.isValid){
             // Email funktion und alert der nach home führt.
-            alert("Die Nachricht wurde erfolgreich gesendet. Der Besitzer wird Sie so schnell wie möglich kontaktieren")
+            emailjs.sendForm('service_pjxcw8w', 'template_xrzmdbc', form.current, 'UjqS-5rIq1BHm8GNV')
+                .then((result) => {
+                    console.log(result.text);
+                }, (error) => {
+                    console.log(error.text);
+                });
+
+            alert("Die Nachricht wurde erfolgreich gesendet. Der Besitzer wird Sie so schnell wie möglich kontaktieren.")
             router.push("/")
         }
         setIsLoading(false)
@@ -71,27 +84,27 @@ export default function KontaktForm() {
 
     return (
         <>
-            <form onSubmit={handleSubmit}>
+            <form ref={form} onSubmit={handleSubmit}>
                 <fieldset className={styles.form}>
-                    <p>Titel:</p>
+                    <p>Vor und Nachname*</p>
                     <Form.Control type="text" name="title" onChange={handleChange} value={formular.title} required/>
                     {errors.title && <div className={styles.error}>{errors.title}</div>}
                 </fieldset>
 
                 <fieldset className={styles.form}>
-                    <p>Nachricht:</p>
+                    <p>Nachricht*</p>
                     <Form.Control name="message" onChange={handleChange} value={formular.message} required/>
                     {errors.message && <div className={styles.error}>{errors.message}</div>}
                 </fieldset>
 
                 <fieldset className={styles.form}>
-                    <p>Reservationszeitpunkt:</p>
+                    <p>Reservationszeitpunkt*</p>
                     <Form.Control type="date" name="reservationTime" onChange={handleChange} value={formular.reservationTime} required/>
                     {errors.reservationTime && <div className={styles.error}>{errors.reservationTime}</div>}
                 </fieldset>
 
                 <fieldset className={styles.form}>
-                    <p>E-Mail:</p>
+                    <p>E-Mail*</p>
                     <Form.Control type="email" name="email" onChange={handleChange} value={formular.email} required/>
                     {errors.email && <div className={styles.error}>{errors.email}</div>}
                 </fieldset>
